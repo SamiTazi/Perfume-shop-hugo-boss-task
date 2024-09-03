bae9f325718c572164e43e93f1177b3dc845333e
SamiTazi <samitazi001@gmail.com>
2024-09-03 22:02:19 +0100

    hi

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+/*<script>
+var prevScrollpos = window.pageYOffset;
+var shop = document.getElementById("sticky-add-to-cart");
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+   shop.style.transform = "translateY(0)";
+  } else {
+    shopstyle.transform = "translateY(-80px)";  
+  }
+  prevScrollpos = currentScrollPos;
+}
+</script>
